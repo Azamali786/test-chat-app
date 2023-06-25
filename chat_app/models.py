8e133f7b76411a868032f26f9c9f9ef60d7bbd27
azam <gausuleazamali@gmail.com>
2023-06-26 01:35:43 +0530

    Feat: added Message model to chat_app

--- chat_app/models.py
+++ chat_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+from chat_app.models import User
+
 # Create your models here.
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sender_id")
+    receiver = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="receiver_id"
+    )
+    message = models.TextField(max_length=1024)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.message
