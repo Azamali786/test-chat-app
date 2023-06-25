72f05fd333b5aaf202deb54805f16c09da868dc7
azam <gausuleazamali@gmail.com>
2023-06-26 00:55:49 +0530

    Feat: added core app to chat_project

--- chat_project/urls.py
+++ chat_project/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path("core/", include("core.urls", namespace="core_app")),
     path("chat/", include("chat_app.urls", namespace="chat_app")),
 ]
---
+++ core/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from chat_app import views
+
+app_name = "core_app"
+
+urlpatterns = [
+    # path("", views.RegistrationView.as_view(), name="home")
+]
