7dbaac91ed04c3429ada202bae78b30d4fc94f48
azam <gausuleazamali@gmail.com>
2023-06-26 01:40:02 +0530

    Added UserAdmin and MessageAdmin

--- chat_app/admin.py
+++ chat_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from chat_app.models import Message
+
+
+class MessaageAdmin(admin.ModelAdmin):
+    list_display = ("receiver_id", "sender_id", "message", "created_at")
+
+
+admin.site.register(Message, MessaageAdmin)
--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from chat_app.models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "email",
+        "gender",
+        "date_of_birth",
+        "contact_number",
+        "is_available",
+        "created_at",
+    )
+
+
+admin.site.register(User, UserAdmin)
